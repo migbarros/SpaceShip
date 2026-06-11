0534675a5a1db1499eb4d955a563580767cb68ab
migbarros <migbarros2011@gmail.com>
2026-06-11 15:48:35 -0300

    agfa

--- SpaceShip/Assets/Scripts/spaw.cs
+++ SpaceShip/Assets/Scripts/spaw.cs
@@ -1,3 +1,5 @@
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class spaw : MonoBehaviour
@@ -5,16 +7,24 @@ public class spaw : MonoBehaviour
     public GameObject prefab;
     [Min(0.5f)]public float spawFrequency;
     private float _timer;
-  
+    public Transform left;
+    public Transform right;
+
     // Update is called once per frame
     void Update()
     {
         _timer += Time.deltaTime;
         if (_timer >= spawFrequency)
         { 
-         Instantiate(prefab, transform.position, transform.rotation);
+         GameObject enemy = Instantiate(prefab, transform);
+            float newX = Random.Range(left.position.x, right.position.x);
+            enemy.transform.position = new Vector2 (newX, transform.position.y);
             _timer = 0;
         
         }
+     }
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
     }
 }
